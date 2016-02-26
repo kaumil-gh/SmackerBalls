32a82e8429f68640303f9f7b742cb5d108cb957e
kaumila4@gmail.com <kaumila4@gmail.com>
2016-02-27 01:19:05 +0530

    Full-Screen Activity
    Load Screen and Home Screen opening in Full Screen Mode.
    Transition between both screen successful.
    
    NOTE:- Java File Name of Full's'creen activity may need to be changed to
    Full'S'creen activity as per system requirements

---
+++ app/src/main/java/com/example/dell/smackerballs/SinglePLayerScreen.java
@@ -0,0 +1,13 @@
+package com.example.dell.smackerballs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SinglePLayerScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_single_player_screen);
+    }
+}
