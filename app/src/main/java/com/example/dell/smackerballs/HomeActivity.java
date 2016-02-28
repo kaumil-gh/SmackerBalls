34a378767ddd7d7c2569afd05608a4f89d4c6e1a
kaumila4@gmail.com <kaumila4@gmail.com>
2016-02-29 00:58:11 +0530

    Flow Designed
    Complete Flow of Game Developed.
    
    NOTE:- Problem Loading Single Player Screen

--- app/src/main/java/com/example/dell/smackerballs/HomeActivity.java
+++ app/src/main/java/com/example/dell/smackerballs/HomeActivity.java
@@ -1,9 +1,11 @@
 package com.example.dell.smackerballs;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import static android.view.WindowManager.LayoutParams.FLAG_FULLSCREEN;
 
@@ -14,5 +16,48 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(FLAG_FULLSCREEN, FLAG_FULLSCREEN);
         setContentView(R.layout.activity_home_2);
+
+        Button button1;
+        Button button2;
+        Button button3;
+        Button button4;
+
+        button1  = (Button)findViewById(R.id.button1);
+        button2  = (Button)findViewById(R.id.button2);
+        button3  = (Button)findViewById(R.id.button3);
+        button4  = (Button)findViewById(R.id.button4);
+
+        button1.setOnClickListener(
+                new Button.OnClickListener() {
+
+                    @Override
+                    public void onClick(View v) {
+                        startActivity(new Intent(getApplicationContext(), SinglePLayerScreen.class));
+                    }
+                });
+        button2.setOnClickListener(
+                new Button.OnClickListener() {
+
+                    @Override
+                    public void onClick(View v) {
+                        startActivity(new Intent(getApplicationContext(), ConnectivityModule.class));
+                    }
+                });
+        button3.setOnClickListener(
+                new Button.OnClickListener() {
+
+                    @Override
+                    public void onClick(View v) {
+                        startActivity(new Intent(getApplicationContext(), SettingsActivity.class));
+                    }
+                });
+        button4.setOnClickListener(
+                new Button.OnClickListener() {
+
+                    @Override
+                    public void onClick(View v) {
+                        startActivity(new Intent(getApplicationContext(), Instructions.class));
+                    }
+                });
     }
 }
--- app/src/main/java/com/example/dell/smackerballs/SinglePLayerScreen.java
+++ app/src/main/java/com/example/dell/smackerballs/SinglePLayerScreen.java
@@ -3,11 +3,14 @@ package com.example.dell.smackerballs;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import static android.view.WindowManager.LayoutParams.FLAG_FULLSCREEN;
+
 public class SinglePLayerScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //getWindow().setFlags(FLAG_FULLSCREEN, FLAG_FULLSCREEN);
         setContentView(R.layout.activity_single_player_screen);
     }
 }
